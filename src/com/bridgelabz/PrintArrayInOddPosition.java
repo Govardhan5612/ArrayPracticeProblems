d5115acef3b642e1c1d9969931febad81707cfe2
Govardhan5612 <govardhansiripireddy5612@gmail.com>
2022-11-24 13:23:50 +0530

    print  odd position elements in array

--- src/com/bridgelabz/PrintArrayInOddPosition.java
+++ src/com/bridgelabz/PrintArrayInOddPosition.java
@@ -9,6 +9,7 @@ public class PrintArrayInOddPosition {
      */
     public static void main(String[] args) {
         int [] array = {0,1,2,3,4,5,6,7,8,9};
+        System.out.print("Print odd position elements : ");
         for (int i=0;i<array.length;i++){
             if (i%2!=0){
                 System.out.print(array[i]+" ");
