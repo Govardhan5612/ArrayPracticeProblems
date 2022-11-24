843cd1b8453974450a518ed29ef6e9d9e87cd3d6
Govardhan5612 <govardhansiripireddy5612@gmail.com>
2022-11-24 10:44:46 +0530

    Print one dimentional array

---
+++ src/com/bridgelabz/PrintOneDimensionalArray.java
@@ -0,0 +1,21 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class PrintOneDimensionalArray {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the length of array : ");
+        int lengthOfArray = input.nextInt();
+        int [] array = new int[lengthOfArray];
+        System.out.println("Enter the values of Array : ");
+        for (int i = 0;i <lengthOfArray;i++){
+            System.out.print("Enter "+(i+1)+" element : ");
+            array[i]=input.nextInt();
+        }
+        System.out.print("Values of array - ");
+        for (int j =0;j<lengthOfArray;j++){
+            System.out.print(array[j]+" ");
+        }
+    }
+}
