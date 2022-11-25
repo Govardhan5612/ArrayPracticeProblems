3de43a6f35ba2bc9c9d0dd79acec0e1f42df5518
Govardhan5612 <govardhansiripireddy5612@gmail.com>
2022-11-25 12:27:44 +0530

    Array values print in ascending Order

--- src/com/bridgelabz/SortElementsOfArray.java
+++ src/com/bridgelabz/SortElementsOfArray.java
@@ -8,10 +8,10 @@ public class SortElementsOfArray {
      *Sorting the elements of Array
      */
     public static void main(String[] args) {
-        int [] array = {0,9,8,4,5,3,5,1,2};
-        int duplicateElement = array[0];
+        int [] array = {10,9,8,7,6,5,8,1};
+        int duplicateElement;
         for (int i=0;i<array.length;i++){
-            for (int j =i+1;j<array.length;j++){
+            for (int j =i;j<array.length;j++){
                 if (array[i]>array[j]){
                     duplicateElement=array[i];
                     array[i]=array[j];
@@ -19,6 +19,7 @@ public class SortElementsOfArray {
                 }
             }
         }
+        System.out.println("Array elements in ascending order : ");
         for (int i =0;i<array.length;i++){
             System.out.print(array[i]+" ");
         }
