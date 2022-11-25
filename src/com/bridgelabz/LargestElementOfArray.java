5681e6f460e6f19fff93b75d83ead36d55a83688
Govardhan5612 <govardhansiripireddy5612@gmail.com>
2022-11-25 12:31:15 +0530

    Print the largest element of the array

--- src/com/bridgelabz/LargestElementOfArray.java
+++ src/com/bridgelabz/LargestElementOfArray.java
@@ -10,18 +10,14 @@ public class LargestElementOfArray {
      */
     public static void main(String[] args) {
         int [] array = {1,2,3,4,5,8,9,6};
-        int duplicateElement = array[0];
+        int largestElement = array[0];
         for (int i=0;i<array.length;i++){
-            for (int j=i+1;j<array.length;j++){
-                if (array[i]<array[j]){
-                    duplicateElement=array[i];
-                    array[i]=array[j];
-                    array[j]=duplicateElement;
-                }
+            if (array[i]>largestElement){
+                largestElement = array[i];
             }
         }
 
-        System.out.println("Largest element of array is "+array[0]);
+        System.out.println("Largest element of array is "+largestElement);
 
     }
 }
